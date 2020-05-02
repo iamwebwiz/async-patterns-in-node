1321f921a55c1d43d7e3d7aa14ff9f826fca15df
Ezekiel <iamwebwiz@gmail.com>
2020-05-02 07:49:57 +0100

    Remove .vscode directory

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -4,14 +4,27 @@ const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
-
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
-
-    res.send(clothingData);
-
+router.route('/').get(function (req, res) {
+  let clothingData = getClothingData((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Returning clothing data');
+      res.send(data);
+    }
   });
+  console.log('Doing more work...');
+});
+
+function getClothingData(callback) {
+  fs.readFile(datafile, 'utf8', (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let clothingData = JSON.parse(data);
+      callback(null, clothingData);
+    }
+  });
+}
 
 module.exports = router;
